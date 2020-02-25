5bbfe28f82ac697d038efb8d777183a787d28c60
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-25 18:20:22 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -11,4 +11,10 @@ class Kid
   end
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  buster = Kid.new
+buster.jump
+// returns "Look how high I'm jumping!"
+buster.take_a_bow
+// returns "Thank you, thank you. It was a pleasure to dance for you all."
+ 
 end
