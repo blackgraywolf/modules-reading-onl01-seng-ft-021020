549360a565069444f30ab3dcd483c7379ac6e7a1
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-25 19:21:19 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,8 +2,9 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 class Dancer
-   include Dance
- 
+  def initialize(name)
+    @name = name
+  end
   attr_accessor :name
  
  
